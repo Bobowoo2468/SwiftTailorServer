f982836be494d7aae01cc3570681813d50881dd0
Bobowoo2468 <woobt123@gmail.com>
2021-08-02 19:02:55 +0800

    ready to deploy final

--- routes/imgAPI.js
+++ routes/imgAPI.js
@@ -3,12 +3,13 @@ const router = express.Router();
 
 router.post("/", function (req, res, next) { 
     imglink = req.body.datas.link;
-    console.log(imglink)
 });
 
 router.get("/", function(req,res,next) {
     const spawn = require("child_process").spawn;
     console.log(gender);
+    console.log(shoulder);
+    console.log(waist);
     const process = spawn('python', ["bodyshape.py", imglink, shoulder, waist, hip, gender]); 
     process.stdout.on('data', function(data){
         console.log(`${data}`); 
