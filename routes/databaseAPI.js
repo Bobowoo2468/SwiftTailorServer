0c0c35da5ef531c75668c2554cb6ab7ec7da19f5
Bobowoo2468 <woobt123@gmail.com>
2021-08-03 18:10:21 +0800

    edit router.post

--- routes/databaseAPI.js
+++ routes/databaseAPI.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const mysql = require('mysql');
-const cors = require('cors');
 
 const db = mysql.createConnection({
   host: 'localhost',
@@ -11,15 +10,7 @@ const db = mysql.createConnection({
   insecureAuth: 'true',
 })
 
-// router.get("/", (req, res) => {
-//     const sqlSelect = "SELECT * FROM fashiondb";
-//     db.query(sqlSelect, (err, result) => {
-//         res.send(result);
-//     })
-// })
-
-
-router.post("/", (req, res) => {
+router.post("/test", (req, res) => {
     const sqlSelect = "SELECT * FROM fashiondb";
     db.query(sqlSelect, (err, result) => {
         res.send(result);
@@ -37,16 +28,5 @@ router.post("/", (req, res) => {
 //   console.log(obj);
 // })
 
-router.post("/test", cors(), function(req,res,next) {
-  const spawn = require("child_process").spawn;
-  const process = spawn('python', ["bodyshape.py", "https://res.cloudinary.com/dnsw7cosi/image/upload/v1627983009/gvvidw9o3qoy1mw5yvzf.jpg", 100, 200, 300, false]); 
-  process.stdout.on('data', function(data){
-      dataToSend = data.toString();
-      console.log(data); 
-      console.log(dataToSend);
-      res.send(dataToSend);
-  });
-
-});
 
 module.exports = router;
