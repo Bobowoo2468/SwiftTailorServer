b82ac39113e6448859af4c31fc8654dd49a9001a
Bobowoo2468 <woobt123@gmail.com>
2021-08-04 15:17:48 +0800

    edit router.post

--- routes/databaseAPI.js
+++ routes/databaseAPI.js
@@ -10,23 +10,11 @@ const db = mysql.createConnection({
   insecureAuth: 'true',
 })
 
-router.post("/test", (req, res) => {
+router.get("/", (req, res) => {
     const sqlSelect = "SELECT * FROM fashiondb";
     db.query(sqlSelect, (err, result) => {
         res.send(result);
     })
 })
-
-// router.post("/test", (req, res) => {
-//   res.send('HELLO');
-//   console.log('HELLO');
-// })
-
-// router.post("/test", (req, res) => {
-//   const obj = {"adult": "Hello, World", "baby" : "Sam"};
-//   res.send(obj);
-//   console.log(obj);
-// })
-
 
 module.exports = router;
--- routes/processAPI.js
+++ routes/processAPI.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.post("/", function(req,res,next) {
+router.get("/", function(req,res,next) {
     const spawn = require("child_process").spawn;
     const process = spawn('python', ["bodyshape.py", imglink, shoulder, waist, hip, gender]); 
     process.stdout.on('data', function(data){
