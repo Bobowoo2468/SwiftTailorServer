f4779169b6a85260cd4e77b0c14baf41f8fa539d
Bobowoo2468 <woobt123@gmail.com>
2021-08-10 01:54:50 +0800

    Create server CRUD

--- routes/databaseAPI.js
+++ routes/databaseAPI.js
@@ -17,4 +17,19 @@ router.get("/", (req, res) => {
     })
 })
 
+router.post ("/update", function(req, res, next) {
+  const name = req.body.data.name;
+  const weblink = req.body.data.weblink;
+  const imagelink = req.body.data.imglink;
+  const bodyshape = req.body.data.bodyshape;
+  const types = req.body.data.types;
+  const price = req.body.data.price;
+  const style = req.body.data.style;
+
+  const sqlInsert = "INSERT INTO fashiondb (name, weblink, imglink, bodyshape, types, price, style) VALUES (?, ?, ?, ?, ?, ?, ?)";
+  db.query(sqlInsert, [name, weblink, imagelink, bodyshape, types, price, style], (err, result) => {
+    console.log(result);
+  })
+});
+
 module.exports = router;
